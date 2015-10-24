ab31854a3431fb01df6a09ce91fef68d60c24e62
krelianer <dauvergne.maxime@gmail.com>
2015-10-24 18:36:03 +0800

    Iniitial unity project
    - TODO : Add Alex C network part
    - TODO : Redo the image for the oculus

---
+++ ROC client/Assets/MainMenuScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PassToMainScene()
+    {
+        if (GameObject.Find("PortField").GetComponent<InputField>().text != "" && GameObject.Find("IpField").GetComponent<InputField>().text != "")
+            Application.LoadLevel("MainScene");
+
+    }
+}
