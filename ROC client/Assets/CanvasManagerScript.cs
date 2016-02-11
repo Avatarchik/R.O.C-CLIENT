4cc6b6de0dbd95e8a4399ed2a1e33d27cdf3b0d4
krelianer <dauvergne.maxime@gmail.com>
2016-02-11 11:32:04 +0100

    Push Travail Design Client
    First test commit, alex a checker

--- ROC client/Assets/CanvasManagerScript.cs
+++ ROC client/Assets/CanvasManagerScript.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using OpenCvSharp;
+using System;
 using System.Collections;
 
 public class CanvasManagerScript : MonoBehaviour {
@@ -22,9 +23,10 @@ public class CanvasManagerScript : MonoBehaviour {
     {
         if (mat.Height != 1920 || mat.Width != 1080)
             mat = ResizeMatTo1920x1080(mat);
-        //Texture2D tempTexture = new Texture2D(mat.Width, mat.Height);
-        //tempTexture.LoadImage(MatToByteArray(mat));
-       // canvasObj.GetComponent<Image>().sprite = Sprite.Create(tempTexture, new UnityEngine.Rect(0, 0, 1920, 1080), Vector2.zero);
+        Debug.Log(System.Environment.Version);
+        Texture2D tempTexture = new Texture2D(mat.Width, mat.Height);
+        tempTexture.LoadImage(MatToByteArray(mat));
+        canvasObj.GetComponent<Image>().sprite = Sprite.Create(tempTexture, new UnityEngine.Rect(0, 0, 1920, 1080), Vector2.zero);
 
         GameObject.Find("TestText").GetComponent<Text>().text = ((int)(1.0f / Time.deltaTime)).ToString();
     }
