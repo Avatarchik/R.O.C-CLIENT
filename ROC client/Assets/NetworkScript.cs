7fb9dab7cbec5d02de5351f8e2b0e9ee4b636c5f
razgort <alexandre.moulay@epitech.eu>
2016-03-10 11:28:36 +0100

    clean du network script Fixe a apporter ds le cas ou le try catch peter retourner sur la scene de base et cloturer la capture

--- ROC client/Assets/NetworkScript.cs
+++ ROC client/Assets/NetworkScript.cs
@@ -7,7 +7,8 @@ using System.Text;
 using System.Configuration;
 using System.Collections.Generic;
 using System.Threading;
-
+using UnityEditor;
+    
 using Emgu.CV.CvEnum;
 using System.Drawing;
 using Emgu.CV;
@@ -18,31 +19,22 @@ using UnityEngine.UI;
 
 public class NetworkScript : MonoBehaviour
 {
-    private String ip = "92.144.127.214"; //ip internet
-    private IPAddress udpAddr;
-    private UdpClient udpClientReceiver;
-    private UdpClient udpClientSender;
-    private TcpClient tcpClientSender;
-    private NetworkStream tcpStream;
-    private int port = 1250;
-
-    private Capture _capture;
+    private String ip = "127.0.0.1"; //ip internet
+    private int port = 0;
+    private String rtspAddr = null;
+    private Capture captureVideo = null;
     private Mat frame;
     private bool mainSceneLoaded;
-
     private CanvasManagerScript canvasScript;
 
 
-    void Awake()
-    {
+    void Awake() {
         DontDestroyOnLoad(this);
     }
 
     // Use this for initialization
-    void Start()
-    {
+    void Start() {
         Debug.Log("Start of Network Script");
-
         frame = new Mat();
         mainSceneLoaded = false;
     }
@@ -53,37 +45,50 @@ public class NetworkScript : MonoBehaviour
         this.ip = GameObject.Find("IpField").GetComponent<InputField>().text;
         this.port = Int32.Parse(GameObject.Find("PortField").GetComponent<InputField>().text);
         Debug.Log("Try connect sender with :\nIP : " + this.ip + "\nPORT : " + this.port);
-        _capture = new Capture(0);
-
+        if (this.ip.Length > 5 && this.port.ToString().Length > 2)
+        {
+            this.rtspAddr = "rtsp://" + this.ip + ":" + this.port + "/camera_0";
+            Debug.Log("rtsp addr is set : " + this.rtspAddr);
+        }
         return (0);
     }
 
     // Function automatically called on every script after a level has been loaded
-    void OnLevelWasLoaded(int level)
-    {
-        if (level == 1)
-        {
+    void OnLevelWasLoaded(int level) {
+        if (level == 1) {
             canvasScript = GameObject.Find("MainSceneManager").GetComponent<CanvasManagerScript>();
             mainSceneLoaded = true;
+            try {
+                if (this.rtspAddr == null)
+                    captureVideo = new Capture(0);
+                else
+                    captureVideo = new Capture(this.rtspAddr);
+            }
+            catch (Exception excpt) {
+                EditorUtility.DisplayDialog("Error", excpt.Message, "ok", "");
+                //capture fail retourner au menu
+            }
         }
-        else if (level == 0)
-        {
-            mainSceneLoaded = false;
-            _capture.Dispose();
+        else if (level == 0) {
+            if (captureVideo != null) {
+                mainSceneLoaded = false;
+                captureVideo.Dispose();
+                captureVideo = null;
+                this.ip = "127.0.0.1";
+                this.port = 0;
+                this.rtspAddr = null;
+            }
         }
     }
 
-    void Update()
-    {
-        if (mainSceneLoaded == true)
-        {
-            _capture.Retrieve(frame, 0);
+    void Update() {
+        if (mainSceneLoaded == true) {
+            captureVideo.Retrieve(frame, 0);
             canvasScript.SetImage(frame);
         }
     }
 
-    void OnDestroy()
-    {
+    void OnDestroy() {
         Debug.Log("On destroy called");
     }
 
