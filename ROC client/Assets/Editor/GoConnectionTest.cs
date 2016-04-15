a9c4a79a2c80814aa132070105b40cf1328583a5
krelianer <dauvergne.maxime@gmail.com>
2016-04-15 15:50:01 +0800

    Adding basic testing for go connection

---
+++ ROC client/Assets/Editor/GoConnectionTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+
+[TestFixture]
+[Category("Connection Tests")]
+ class GoConnectionTest
+{
+    CommunicationManagerScript script = new CommunicationManagerScript();
+
+    [Test]
+    [Category("Unknown Tests")]
+    public void FirstConnectionTest()
+    {
+            Assert.AreNotEqual(-1, script.StartGoLink());
+    }
+
+}
